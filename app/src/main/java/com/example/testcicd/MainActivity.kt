57f3c78e4c5ad903c20ae1976d0470e602a822b2
Huy Dao Duc <huydd.1997@gmail.com>
2023-03-08 21:40:28 +0700

    push test CICD

--- app/src/main/java/com/example/testcicd/MainActivity.kt
+++ app/src/main/java/com/example/testcicd/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.testcicd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        var textView: TextView = findViewById(R.id.txtCICD)
+
+        textView.setText("CICD")
     }
 }
